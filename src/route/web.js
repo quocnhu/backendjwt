ed42dfbfb8a8a8bb5fc839aef748ae736568118a
mr nhu95 <nquocnhu95@gmail.com>
2024-06-06 16:54:09 +0700

    backend is done

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,14 @@
-import {homePage,welcome} from "../../src/controller/homeController.js";
+import {handleCreateNewUser,handleDeleteUser,handleUserPage,getUserUpdatePage,handleUpdateUser} from "../../src/controller/homeController.js";
 // import express from 'express';
 import express from 'express';
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-    router.get('/',homePage)
-    router.get('/wc',welcome)
+    router.get('/',handleUserPage)
+    router.post('/create-user',handleCreateNewUser)
+    router.post('/delete-user/:id',handleDeleteUser)
+    router.get('/updatePage/:id',getUserUpdatePage)
+    router.post('/handleUpdateUser',handleUpdateUser)
 
     return (app.use('/', router))
 }
